1144e61c3b7dfdfc16dde616e7c23cf3c104856a
Boris Martínez <borislasky@gmail.com>
2019-08-16 09:55:44 +0200

    más cambios

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("hola");
-  miwifi.IpEstatica("192.168.43.111", "255.255.255.0", "192.168.43.1");
+  miwifi.IpEstatica((char *)"192.168.43.111",(char *)"255.255.255.0", (char *)"192.168.43.1");
   miwifi.ConectaWiFi();
 }
 
